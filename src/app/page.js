7280c9406f587769e6f780f2257b1581bd2c48f5
Novelja <novelja@gmail.com>
2024-04-17 13:28:23 +0200

    init hero

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,21 @@
 import Image from 'next/image';
 import { Button } from '@mui/material';
+import Hero from '@/components/sections/Hero';
 
 export default function Home() {
     return (
         <main>
             <Button>Test</Button>
+
+            <Hero
+                backgroundImage={''}
+                headline={'THE ESTÉE LAUDER COMPAGNIES'}
+                subhead={
+                    'New incubation ventures signature program for beauty start-ups in the world.'
+                }
+                ctaText={'LEARN MORE'}
+                ctaOnClick={() => {}}
+            />
         </main>
     );
 }
---
+++ src/components/sections/Hero.js
@@ -0,0 +1,10 @@
+import { Box, Typography } from '@mui/material';
+import React from 'react';
+
+export default function Hero() {
+    return (
+        <Box>
+            <Typography variant="h1">HERO</Typography>
+        </Box>
+    );
+}
