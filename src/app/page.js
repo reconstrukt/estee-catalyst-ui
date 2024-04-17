af2fc51689f89b14d5da8e3038200ca3398f6907
Novelja <novelja@gmail.com>
2024-04-17 18:40:56 +0200

    cleanup

--- src/app/page.js
+++ src/app/page.js
@@ -7,8 +7,6 @@ import Link from 'next/link';
 export default function Home() {
     return (
         <main>
-            <Button>Test</Button>
-
             <Hero backgroundImage={HeroBg.src}>
                 <Box
                     sx={{
