a520395f73bd2a4252e9dad5b25318e8bcc76263
Novelja <novelja@gmail.com>
2024-04-23 13:30:15 +0200

    faq accordion

---
+++ src/app/faq/page.js
@@ -0,0 +1,95 @@
+import {
+    Box,
+    Typography,
+    Stack,
+    Accordion,
+    AccordionSummary,
+    AccordionDetails,
+} from '@mui/material';
+import Hero from '@/components/sections/Hero';
+import PageLayout from '@/components/layouts/PageLayout';
+
+import FaqImage from '../../../assets/images/faq.jpg';
+
+export default function FaqPage() {
+    return (
+        <PageLayout transparentHeader={false}>
+            <Hero
+                sx={{
+                    minHeight: '85vh',
+                }}
+                backgroundImage={FaqImage.src}
+            >
+                <Box
+                    sx={{
+                        textAlign: 'center',
+                        color: 'white.main',
+                        maxWidth: 1164,
+                    }}
+                >
+                    <Typography variant="h1" mb={1}>
+                        FAQ
+                    </Typography>
+                </Box>
+            </Hero>
+
+            <Box>
+                <Accordion>
+                    <AccordionSummary>
+                        How do you define a premium beauty brand?
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        “Premium” is more than just the price of products and
+                        manifests itself differently across geographies and as
+                        consumers and channels evolve. We look for beauty brands
+                        that elevate the story, the packaging, the products and
+                        the entire brand experience, which combine to create a
+                        compelling brand proposition for consumers.
+                    </AccordionDetails>
+                </Accordion>
+
+                <Accordion>
+                    <AccordionSummary>
+                        How do you define a premium beauty brand?
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        “Premium” is more than just the price of products and
+                        manifests itself differently across geographies and as
+                        consumers and channels evolve. We look for beauty brands
+                        that elevate the story, the packaging, the products and
+                        the entire brand experience, which combine to create a
+                        compelling brand proposition for consumers.
+                    </AccordionDetails>
+                </Accordion>
+
+                <Accordion>
+                    <AccordionSummary>
+                        How do you define a premium beauty brand?
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        “Premium” is more than just the price of products and
+                        manifests itself differently across geographies and as
+                        consumers and channels evolve. We look for beauty brands
+                        that elevate the story, the packaging, the products and
+                        the entire brand experience, which combine to create a
+                        compelling brand proposition for consumers.
+                    </AccordionDetails>
+                </Accordion>
+
+                <Accordion>
+                    <AccordionSummary>
+                        How do you define a premium beauty brand?
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        “Premium” is more than just the price of products and
+                        manifests itself differently across geographies and as
+                        consumers and channels evolve. We look for beauty brands
+                        that elevate the story, the packaging, the products and
+                        the entire brand experience, which combine to create a
+                        compelling brand proposition for consumers.
+                    </AccordionDetails>
+                </Accordion>
+            </Box>
+        </PageLayout>
+    );
+}
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -48,55 +48,65 @@ const palette = {
     },
 };
 
+const typography = {
+    fontFamily: GaramondFont.style.fontFamily,
+
+    h1: {
+        fontFamily: HelveticaNeueFont.style.fontFamily,
+
+        fontSize: 110,
+        fontWeight: 700,
+        lineHeight: '88px',
+        letterSpacing: '-0.015em',
+    },
+    h2: {
+        fontFamily: HelveticaNeueFont.style.fontFamily,
+
+        fontSize: 42,
+        fontWeight: 700,
+        lineHeight: '37.8px',
+        letterSpacing: '-0.015em',
+    },
+    h3: {
+        fontFamily: HelveticaNeueFont.style.fontFamily,
+    },
+    h4: {
+        fontFamily: HelveticaNeueFont.style.fontFamily,
+    },
+    h5: {
+        fontFamily: HelveticaNeueFont.style.fontFamily,
+    },
+    h6: {
+        fontFamily: HelveticaNeueFont.style.fontFamily,
+    },
+
+    subtitle1: {
+        fontSize: 48,
+        lineHeight: '45.6px',
+        letterSpacing: '-0.02em',
+    },
+
+    button: {
+        fontFamily: HelveticaNowFont.style.fontFamily,
+        fontSize: 16,
+    },
+
+    body1: {
+        fontSize: 24,
+    },
+
+    body2: {
+        fontFamily: HelveticaNowFont.style.fontFamily,
+        fontSize: 16,
+        fontWeight: 400,
+        lineHeight: '20.8px',
+        textAlign: 'center',
+    },
+};
+
 const theme = createTheme({
     palette,
-    typography: {
-        fontFamily: GaramondFont.style.fontFamily,
-
-        h1: {
-            fontFamily: HelveticaNeueFont.style.fontFamily,
-
-            fontSize: 110,
-            fontWeight: 700,
-            lineHeight: '88px',
-            letterSpacing: '-0.015em',
-        },
-        h2: {
-            fontFamily: HelveticaNeueFont.style.fontFamily,
-
-            fontSize: 42,
-            fontWeight: 700,
-            lineHeight: '37.8px',
-            letterSpacing: '-0.015em',
-        },
-        h3: {
-            fontFamily: HelveticaNeueFont.style.fontFamily,
-        },
-        h4: {
-            fontFamily: HelveticaNeueFont.style.fontFamily,
-        },
-        h5: {
-            fontFamily: HelveticaNeueFont.style.fontFamily,
-        },
-        h6: {
-            fontFamily: HelveticaNeueFont.style.fontFamily,
-        },
-
-        subtitle1: {
-            fontSize: 48,
-            lineHeight: '45.6px',
-            letterSpacing: '-0.02em',
-        },
-
-        button: {
-            fontFamily: HelveticaNowFont.style.fontFamily,
-            fontSize: 16,
-        },
-
-        body1: {
-            fontSize: 24,
-        },
-    },
+    typography,
 
     components: {
         MuiButton: {
@@ -107,6 +117,58 @@ const theme = createTheme({
 
                 outlinedWhite: {
                     borderColor: palette.white.main,
+                },
+            },
+        },
+
+        MuiAccordion: {
+            defaultProps: {
+                elevation: 0,
+            },
+
+            styleOverrides: {
+                root: {
+                    borderBottom: '1px solid black',
+                    borderBottomLeftRadius: '0 !important',
+                    borderBottomRightRadius: '0 !important',
+                    borderTopLeftRadius: '0 !important',
+                    borderTopRightRadius: '0 !important',
+
+                    '&.Mui-expanded': {
+                        margin: '0 !important', // overwritte default styles
+                    },
+                },
+            },
+        },
+
+        MuiAccordionSummary: {
+            styleOverrides: {
+                root: {
+                    ...typography.body1,
+                },
+
+                content: {
+                    justifyContent: 'center',
+                    textAlign: 'center',
+
+                    '&.Mui-expanded': {
+                        margin: '12px 0 !important', // overwritte default styles
+                    },
+                },
+            },
+        },
+
+        MuiAccordionDetails: {
+            styleOverrides: {
+                root: {
+                    ...typography.body2,
+                    maxWidth: 698,
+                    margin: '0 auto',
+                },
+
+                content: {
+                    justifyContent: 'center',
+                    textAlign: 'center',
                 },
             },
         },
