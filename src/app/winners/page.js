07e45ef33807df652b3228c35d5b8a60896da3d7
Novelja <novelja@gmail.com>
2024-04-25 19:06:35 +0200

    winners

---
+++ src/app/winners/page.js
@@ -0,0 +1,135 @@
+import { Box, Typography, Stack } from '@mui/material';
+import Hero from '@/components/sections/Hero';
+import PageLayout from '@/components/layouts/PageLayout';
+
+import PressImage from '../../../assets/images/press.jpg';
+import winners from '@/cms/winners';
+import Link from 'next/link';
+
+export default function ContactUs() {
+    return (
+        <PageLayout>
+            <Hero backgroundImage={PressImage.src}>
+                <Box
+                    sx={{
+                        textAlign: 'center',
+                        color: 'white.main',
+                        maxWidth: 1164,
+                    }}
+                >
+                    <Typography variant="h1" mb={1}>
+                        THE WINNERS
+                    </Typography>
+                    <Typography variant="subtitle1">
+                        Meet the 2024 Winners
+                    </Typography>
+                </Box>
+            </Hero>
+
+            <Box>
+                {winners.map((item, i) => (
+                    <Box key={i}>
+                        <Stack
+                            sx={{
+                                flexDirection: {
+                                    xs: 'column',
+                                    md: i % 2 === 0 ? 'row' : 'row-reverse',
+                                },
+                            }}
+                        >
+                            <Box
+                                sx={{
+                                    minHeight: {
+                                        xs: '100vw',
+                                        md: '100vh',
+                                    },
+                                    flex: {
+                                        md: '1 0 50%',
+                                    },
+                                    backgroundImage: `url(${item.image.src})`,
+                                    backgroundSize: 'cover',
+                                    backgroundPosition: 'center',
+
+                                    textAlign: 'center',
+                                }}
+                            ></Box>
+
+                            <Box
+                                sx={{
+                                    flex: {
+                                        md: '1 0 50%',
+                                    },
+                                    px: {
+                                        xs: 2,
+                                    },
+                                    py: {
+                                        xs: 10,
+                                    },
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                }}
+                            >
+                                <Stack
+                                    spacing={10}
+                                    sx={{
+                                        maxWidth: 680,
+                                    }}
+                                >
+                                    <Stack alignItems="center" spacing={0.5}>
+                                        {item.preTitle && (
+                                            <Typography variant="button">
+                                                {item.preTitle}
+                                            </Typography>
+                                        )}
+                                        {item.title && (
+                                            <Typography variant="h2">
+                                                {item.title}
+                                            </Typography>
+                                        )}
+                                        {item.postTitle && (
+                                            <Typography variant="button">
+                                                {item.postTitle}
+                                            </Typography>
+                                        )}
+                                    </Stack>
+
+                                    <Stack
+                                        sx={{
+                                            textAlign: 'center',
+                                        }}
+                                    >
+                                        {item.copy && (
+                                            <Typography variant="body1">
+                                                {item.copy}
+                                            </Typography>
+                                        )}
+
+                                        {item.links && (
+                                            <Stack
+                                                mt={2}
+                                                spacing={2}
+                                                direction="row"
+                                                justifyContent="center"
+                                                sx={{
+                                                    textTransform: 'uppercase',
+                                                    textDecoration: 'underline',
+                                                }}
+                                            >
+                                                {item.links.map((link, j) => (
+                                                    <Link href={link.href}>
+                                                        {link.text}
+                                                    </Link>
+                                                ))}
+                                            </Stack>
+                                        )}
+                                    </Stack>
+                                </Stack>
+                            </Box>
+                        </Stack>
+                    </Box>
+                ))}
+            </Box>
+        </PageLayout>
+    );
+}
---
+++ src/cms/winners.js
@@ -0,0 +1,117 @@
+import winner1 from '../../assets/winners/winner1.jpg';
+
+const winners = [
+    {
+        image: winner1,
+        preTitle: 'BREAKTHROUGH AWARD WINNER',
+        title: 'CLÉMENTINE DEROZI',
+        postTitle: 'Biome Laboratories',
+
+        copy: `As Executive Director & CEO, Anchit spearheads the flagship beauty e-commerce and retail businesses, holistically focusing on industry-first 
+        initiatives and best-in-class omnichannel experiences tailored to the needs of both customers and brand partners. Anchit's journey with 
+        Nykaa began as CEO-Nykaa Retail to lead the company's offline strategy with a focus on expanding the physical store presence. Today, along with highly differentiated platforms, Nykaa's retail footprint spans across 145 stores in 60 cities, offering an extensive array of over 3400+ Indian 
+        and international brands. Under his stewardship, Nykaa continues to be a dominant and innovative marketing force, redefining beauty retail 
+        in the country and bringing the best trends, innovations and experiences closer to consumers.`,
+
+        links: [
+            {
+                text: 'Website',
+                href: '#',
+            },
+            {
+                text: 'INSTAGRAM',
+                href: '#',
+            },
+            {
+                text: 'WATCH NOW',
+                href: '#',
+            },
+        ],
+    },
+
+    {
+        image: winner1,
+        preTitle: 'BREAKTHROUGH AWARD WINNER',
+        title: 'CLÉMENTINE DEROZI',
+        postTitle: 'Biome Laboratories',
+
+        copy: `As Executive Director & CEO, Anchit spearheads the flagship beauty e-commerce and retail businesses, holistically focusing on industry-first 
+        initiatives and best-in-class omnichannel experiences tailored to the needs of both customers and brand partners. Anchit's journey with 
+        Nykaa began as CEO-Nykaa Retail to lead the company's offline strategy with a focus on expanding the physical store presence. Today, along with highly differentiated platforms, Nykaa's retail footprint spans across 145 stores in 60 cities, offering an extensive array of over 3400+ Indian 
+        and international brands. Under his stewardship, Nykaa continues to be a dominant and innovative marketing force, redefining beauty retail 
+        in the country and bringing the best trends, innovations and experiences closer to consumers.`,
+
+        links: [
+            {
+                text: 'Website',
+                href: '#',
+            },
+            {
+                text: 'INSTAGRAM',
+                href: '#',
+            },
+            {
+                text: 'WATCH NOW',
+                href: '#',
+            },
+        ],
+    },
+
+    {
+        image: winner1,
+        preTitle: 'BREAKTHROUGH AWARD WINNER',
+        title: 'CLÉMENTINE DEROZI',
+        postTitle: 'Biome Laboratories',
+
+        copy: `As Executive Director & CEO, Anchit spearheads the flagship beauty e-commerce and retail businesses, holistically focusing on industry-first 
+        initiatives and best-in-class omnichannel experiences tailored to the needs of both customers and brand partners. Anchit's journey with 
+        Nykaa began as CEO-Nykaa Retail to lead the company's offline strategy with a focus on expanding the physical store presence. Today, along with highly differentiated platforms, Nykaa's retail footprint spans across 145 stores in 60 cities, offering an extensive array of over 3400+ Indian 
+        and international brands. Under his stewardship, Nykaa continues to be a dominant and innovative marketing force, redefining beauty retail 
+        in the country and bringing the best trends, innovations and experiences closer to consumers.`,
+
+        links: [
+            {
+                text: 'Website',
+                href: '#',
+            },
+            {
+                text: 'INSTAGRAM',
+                href: '#',
+            },
+            {
+                text: 'WATCH NOW',
+                href: '#',
+            },
+        ],
+    },
+
+    {
+        image: winner1,
+        preTitle: 'BREAKTHROUGH AWARD WINNER',
+        title: 'CLÉMENTINE DEROZI',
+        postTitle: 'Biome Laboratories',
+
+        copy: `As Executive Director & CEO, Anchit spearheads the flagship beauty e-commerce and retail businesses, holistically focusing on industry-first 
+        initiatives and best-in-class omnichannel experiences tailored to the needs of both customers and brand partners. Anchit's journey with 
+        Nykaa began as CEO-Nykaa Retail to lead the company's offline strategy with a focus on expanding the physical store presence. Today, along with highly differentiated platforms, Nykaa's retail footprint spans across 145 stores in 60 cities, offering an extensive array of over 3400+ Indian 
+        and international brands. Under his stewardship, Nykaa continues to be a dominant and innovative marketing force, redefining beauty retail 
+        in the country and bringing the best trends, innovations and experiences closer to consumers.`,
+
+        links: [
+            {
+                text: 'Website',
+                href: '#',
+            },
+            {
+                text: 'INSTAGRAM',
+                href: '#',
+            },
+            {
+                text: 'WATCH NOW',
+                href: '#',
+            },
+        ],
+    },
+];
+
+export default winners;
