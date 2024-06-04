e5f58e0828a54a3c869b74033b1cc8b5d17faf69
Novelja <novelja@gmail.com>
2024-06-04 14:09:09 +0200

    faq

--- src/app/faq/page.js
+++ src/app/faq/page.js
@@ -8,6 +8,7 @@ import {
 } from '@mui/material';
 import Hero from '@/components/sections/Hero';
 import PageLayout from '@/components/layouts/PageLayout';
+import Link from 'next/link';
 
 import faqmob from '../../../assets/images/faqmob.jpg';
 import faq from '../../../assets/images/faq.jpg';
@@ -36,60 +37,117 @@ export default function FaqPage() {
                 </Box>
             </Hero>
 
-            <Box>
+            <Box
+                sx={{
+                    textAlign: 'left',
+
+                    a: {
+                        textDecoration: 'underline',
+                    },
+                }}
+            >
                 <Accordion>
                     <AccordionSummary>
-                        How do you define a premium beauty brand?
+                        Who is eligible to apply?
                     </AccordionSummary>
                     <AccordionDetails>
-                        “Premium” is more than just the price of products and
-                        manifests itself differently across geographies and as
-                        consumers and channels evolve. We look for beauty brands
-                        that elevate the story, the packaging, the products and
-                        the entire brand experience, which combine to create a
-                        compelling brand proposition for consumers.
+                        <Box
+                            sx={{
+                                ul: {
+                                    paddingLeft: '20px',
+                                    textAlign: 'left',
+                                },
+                            }}
+                        >
+                            Participation is open only to creators who are legal
+                            residents of their country of residence, are at
+                            least 21 years of age and have attained the age of
+                            majority in their state, country or province of
+                            residence. RESIDENTS OF ANY COUNTRY CURRENTLY
+                            EMBARGOED OR SANCTIONED BY THE UNITED STATES ARE NOT
+                            ELIGIBLE TO PARTICIPATE IN THE CONTEST AND THE
+                            CONTEST IS VOID IN THESE JURISDICTIONS AND WHERE
+                            PROHIBITED (COLLECTIVELY, “INELIGIBLE COUNTRIES”).
+                            For the full eligibility requirements, please review
+                            the{' '}
+                            <Link href="/terms-and-conditions">
+                                Terms & Conditions
+                            </Link>{' '}
+                            <br />
+                            <br />
+                            Applications should be in the beauty category and
+                            can fall under any one or more of the below
+                            subcategories:
+                            <ul>
+                                <li>Makeup</li>
+                                <li>Skin care</li>
+                                <li>Body care</li>
+                                <li>Hair care</li>
+                                <li>Fragrance</li>
+                                <li>Home fragrance</li>
+                                <li>
+                                    Creative storytelling – a visual concept,
+                                    for example an exhibition, book, or film{' '}
+                                </li>
+                            </ul>
+                        </Box>
                     </AccordionDetails>
                 </Accordion>
 
                 <Accordion>
                     <AccordionSummary>
-                        How do you define a premium beauty brand?
+                        Who decides the winners?
                     </AccordionSummary>
                     <AccordionDetails>
-                        “Premium” is more than just the price of products and
-                        manifests itself differently across geographies and as
-                        consumers and channels evolve. We look for beauty brands
-                        that elevate the story, the packaging, the products and
-                        the entire brand experience, which combine to create a
-                        compelling brand proposition for consumers.
+                        The Catalysts is being judged by an esteemed jury of
+                        creative figures, including accomplished entrepreneurs,
+                        investors, content creators, experts, and
+                        representatives from The Estée Lauder Companies and
+                        TIkTok. To see the full jury, please{' '}
+                        <Link href="/judges">click here</Link>.
                     </AccordionDetails>
                 </Accordion>
 
                 <Accordion>
                     <AccordionSummary>
-                        How do you define a premium beauty brand?
+                        What can I do with the award money?
                     </AccordionSummary>
                     <AccordionDetails>
-                        “Premium” is more than just the price of products and
-                        manifests itself differently across geographies and as
-                        consumers and channels evolve. We look for beauty brands
-                        that elevate the story, the packaging, the products and
-                        the entire brand experience, which combine to create a
-                        compelling brand proposition for consumers.
+                        Award money should be used to further the development of
+                        the idea.
                     </AccordionDetails>
                 </Accordion>
 
                 <Accordion>
                     <AccordionSummary>
-                        How do you define a premium beauty brand?
+                        I have an idea for a brand/product/etc but I haven't
+                        launched it yet, am I eligible?
                     </AccordionSummary>
                     <AccordionDetails>
-                        “Premium” is more than just the price of products and
-                        manifests itself differently across geographies and as
-                        consumers and channels evolve. We look for beauty brands
-                        that elevate the story, the packaging, the products and
-                        the entire brand experience, which combine to create a
-                        compelling brand proposition for consumers.
+                        Yes! We welcome applications about brands, products, or
+                        services that you need support in getting to market.
+                    </AccordionDetails>
+                </Accordion>
+
+                <Accordion>
+                    <AccordionSummary>
+                        What’s the deadline for applications?
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        11:59:59 PM ET on July 29, 2024
+                    </AccordionDetails>
+                </Accordion>
+
+                <Accordion>
+                    <AccordionSummary>
+                        I have more questions! Who can I contact?
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        Please reach out to us at{' '}
+                        <a href="mailto:hello@thecatalystsworld.com">
+                            hello@thecatalystsworld.com
+                        </a>
+                        .
                     </AccordionDetails>
                 </Accordion>
             </Box>
