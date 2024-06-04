e57be11e29b5f33f7a6e5662c9ca161c9df9e05c
Novelja <novelja@gmail.com>
2024-06-04 14:32:00 +0200

    judges

--- src/cms/judges.js
+++ src/cms/judges.js
@@ -1,78 +1,95 @@
-import annemarie from '../../assets/judges/annemarie.jpg';
-import clementine from '../../assets/judges/clementine.jpg';
-import martin from '../../assets/judges/martin.jpg';
+import carlos from 'assets/judges/carlos.webp';
+import courtney from 'assets/judges/courtney.webp';
+import hung from 'assets/judges/hung.webp';
+import ruba from 'assets/judges/ruba.webp';
+import sallyann from 'assets/judges/sallyann.webp';
+import shana from 'assets/judges/shana.webp';
+import thomas from 'assets/judges/thomas.webp';
+import vanessa from 'assets/judges/vanessa.webp';
 
 const judges = [
     {
-        image: annemarie,
-        name: 'ANNE-MARIE LECLERC',
-        title: (
-            <>
-                Founder and CEO,
-                <br />
-                Prakti Beauty.
-            </>
-        ),
-
-        copy: `As Executive Director & CEO, Anchit spearheads the flagship beauty e-commerce and retail businesses, holistically focusing on industry-first 
-        initiatives and best-in-class omnichannel experiences tailored to the needs of both customers and brand partners. Anchit's journey with 
-        Nykaa began as CEO-Nykaa Retail to lead the company's offline strategy with a focus on expanding the physical store presence. Today, along with highly differentiated platforms, Nykaa's retail footprint spans across 145 stores in 60 cities, offering an extensive array of over 3400+ Indian 
-        and international brands. Under his stewardship, Nykaa continues to be a dominant and innovative marketing force, redefining beauty retail 
-        in the country and bringing the best trends, innovations and experiences closer to consumers.`,
+        image: hung,
+        name: 'HUNG VANNGO',
+        copy: `Hung Vanngo is a Vietnam-born, New York-based makeup artist known for combining glamour with an avant-garde aesthetic. He’s a go-to for prestigious brands such as Louis Vuitton, Marc Jacobs, and Calvin Klein, as well as high profile talent such as Selena Gomez, Bella Hadid, and Gwyneth Paltrow.`,
     },
 
     {
-        image: clementine,
-        name: 'CLÉMENTINE DEROZI',
-        title: (
-            <>
-                Founder and CEO,
-                <br />
-                Prakti Beauty.
-            </>
-        ),
-
-        copy: `As Executive Director & CEO, Anchit spearheads the flagship beauty e-commerce and retail businesses, holistically focusing on industry-first 
-        initiatives and best-in-class omnichannel experiences tailored to the needs of both customers and brand partners. Anchit's journey with 
-        Nykaa began as CEO-Nykaa Retail to lead the company's offline strategy with a focus on expanding the physical store presence. Today, along with highly differentiated platforms, Nykaa's retail footprint spans across 145 stores in 60 cities, offering an extensive array of over 3400+ Indian 
-        and international brands. Under his stewardship, Nykaa continues to be a dominant and innovative marketing force, redefining beauty retail 
-        in the country and bringing the best trends, innovations and experiences closer to consumers.`,
+        image: '',
+        name: 'EUGENE SOULEIMAN',
+        copy: `Originally inspired by London’s punk scene, Eugene Souleiman is a hairstylist who creates with raw emotion and technical ability. Over a career spanning more than 30 years, his unique perspective has led him to work with a diverse group of the most forward-thinking and innovative fashion designers, including John Galliano, Alexander McQueen, and Yohji Yamamoto.`,
     },
 
     {
-        image: martin,
-        name: 'MARTIN DUPONT',
-        title: (
-            <>
-                Founder and CEO,
-                <br />
-                Prakti Beauty.
-            </>
-        ),
-
-        copy: `As Executive Director & CEO, Anchit spearheads the flagship beauty e-commerce and retail businesses, holistically focusing on industry-first 
-        initiatives and best-in-class omnichannel experiences tailored to the needs of both customers and brand partners. Anchit's journey with 
-        Nykaa began as CEO-Nykaa Retail to lead the company's offline strategy with a focus on expanding the physical store presence. Today, along with highly differentiated platforms, Nykaa's retail footprint spans across 145 stores in 60 cities, offering an extensive array of over 3400+ Indian 
-        and international brands. Under his stewardship, Nykaa continues to be a dominant and innovative marketing force, redefining beauty retail 
-        in the country and bringing the best trends, innovations and experiences closer to consumers.`,
+        image: vanessa,
+        name: 'VANESSA CRAFT',
+        copy: `As TikTok’s Global Head of Content Partnerships, Vanessa Craft is a champion of next-gen talent. She is responsible for building engaged communities, directing game-changing content, and driving innovative business strategies for TikTok’s top Global partners across Fashion, Entertainment, Lifestyle, Education and Sports – as well as spearheading TikTok's first sponsorship of the 2024 Met Gala.`,
     },
 
     {
-        image: annemarie,
-        name: 'CLÉMENTINE SNOW',
-        title: (
-            <>
-                Founder and CEO,
-                <br />
-                Prakti Beauty.
-            </>
-        ),
+        image: '',
+        name: 'VIOLET CHACHKI',
+        copy: `The winner of RuPaul’s Drag Race season seven, Violet Chachki is a trailblazing drag queen, burlesque performer, aerial artist, model and recording artist. The first drag artist to attend the Met Gala, Chachki has collaborated with leading brands including Prada, Moschino, Jean Paul Gaultier, and Miu Miu, and this summer will have a residency at famed Parisian cabaret club Crazy Horse.`,
+    },
 
-        copy: `As Executive Director & CEO, Anchit spearheads the flagship beauty e-commerce and retail businesses, holistically focusing on industry-first 
-        initiatives and best-in-class omnichannel experiences tailored to the needs of both customers and brand partners. Anchit's journey with 
-        Nykaa began as CEO-Nykaa Retail to lead the company's offline strategy with a focus on expanding the physical store presence. Today, along with highly differentiated platforms, Nykaa's retail footprint spans across 145 stores in 60 cities, offering an extensive array of over 3400+ Indian 
-        and international brands. Under his stewardship, Nykaa continues to be a dominant and innovative marketing force, redefining beauty retail 
-        in the country and bringing the best trends, innovations and experiences closer to consumers.`,
+    {
+        image: ruba,
+        name: 'RUBA ABU-NIMAH',
+        copy: `Ruba Abu-Nimah is an award-winning New York-based creative director, design and experiential curator, editor, and currently Brand Creative Director of Moncler. Ruba has held lead creative positions including Executive Creative Director of Tiffany & Co., Global Creative Director at Revlon and Shiseido, and Creative Director of ELLE Magazine.`,
+    },
+
+    {
+        image: '',
+        name: 'MICHELE LAMY',
+        copy: `Michèle Lamy is an artist, entrepreneur, producer, collaborator, performer and the co-founder of Owenscorp, along with her life partner Rick Owens. In 2014, Lamy created 'LAMYLAND,' an umbrella term for her independent creative endeavours, bringing together experimentation, collaboration, storytelling, and creation. She has worked on projects at renowned institutions and art fairs, including the Museum of Contemporary Art, Los Angeles, Frieze London, the Venice Biennale, and more.`,
+    },
+
+    {
+        image: thomas,
+        name: 'THOMAS ROUSSEL',
+        copy: `Thomas Roussel is a French composer and conductor based in Paris. For over twenty years, he has worked across fashion, cinema and luxury, including creating soundtracks for brands including Chanel, Dior, Givenchy, Valentino, and Cartier. He recently served as orchestra conductor and musical director for Pharrell Williams' first two shows at Louis Vuitton.`,
+    },
+
+    {
+        image: '',
+        name: 'FRANCISCO COSTA',
+        copy: `After serving as the creative director of Calvin Klein womenswear for 13 years, in 2016 Francisco Costa returned to his home country of Brazil. There, he launched Costa Brazil, an eco-forward skincare and fragrance line inspired by local healing rituals that works in partnership with local cooperatives and scientists, ensuring protection of both body and Earth. As well as being an avid environmentalist, Costa is engaged in numerous social causes in Brazil and around the world.`,
+    },
+
+    {
+        image: '',
+        name: 'JULIA SARR-JAMOIS',
+        copy: `With over 16 years experience in the industry, Julia Sarr-Jamois is a stylist and fashion director known for her effortless mixing of streetwear and high fashion aesthetics. With years at both i-D magazine and British Vogue under her belt, she’s shot with photographers including Tyrone Lebon, Alasdair McLellan, Tyler Mitchell, and Harley Weir; collaborated with designers to create custom looks for Beyoncé; and consulted for brands including Homer, Hermes, Chanel, Zara and Nike. `,
+    },
+
+    {
+        image: '',
+        name: 'FABIEN BARON',
+        copy: `The founder of Baron&Baron, Fabien Baron has been leading iconic creative for some of the world’s most celebrated brands for over three decades. He’s spearheaded the image of fashion houses including Calvin Klein, Balenciaga, and Burberry, reinvented illustrious magazines including Interview, French Vogue, Harper’s Bazaar, and Italian Vogue, and creative directed campaigns for the likes of Nars, Louis Vuitton, and Dior. `,
+    },
+
+    {
+        image: carlos,
+        name: 'CARLOS NAZARIA',
+        copy: `Named by the CFDA as one of the stylists shaping the future of fashion, Carlos Nazario is a fashion editor, stylist and creative consultant based in New York City. He has collaborated with esteemed photographers like Alasdair McLellan, Mert Alas & Marcus Piggott, Mario Sorrenti, and Glen Luchford; talent such as Rihanna, Rosalía, Bella Hadid, Naomi Campbell, and Frank Ocean; and magazines including American Vogue, Italian Vogue, and i-D. He is currently the Style Director at Large of Harper’s Bazaar US.`,
+    },
+
+    {
+        image: '',
+        name: 'COURTNEY MCHUGH',
+        copy: ``,
+    },
+
+    {
+        image: '',
+        name: 'ISAMAYA FFRENCH',
+        copy: ``,
+    },
+
+    {
+        image: sallyann,
+        name: 'SALLYANN HOUGHTON',
+        copy: ``,
     },
 ];
 
