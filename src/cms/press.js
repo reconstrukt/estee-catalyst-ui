a9c2df160d2c228c53a05ed98b9811ab2efb6233
Novelja <novelja@gmail.com>
2024-06-04 15:03:07 +0200

    press

--- src/cms/press.js
+++ src/cms/press.js
@@ -7,6 +7,27 @@ const press = [
         title: 'Lorem ipsum dolor sit amet, ne has officiis inciderint',
         link: '#',
     },
+
+    {
+        logo: <Vogue />,
+        date: 'DEC 28, 2023',
+        title: 'Lorem ipsum dolor sit amet, ne has officiis inciderint',
+        link: '#',
+    },
+
+    {
+        logo: <Vogue />,
+        date: 'DEC 28, 2023',
+        title: 'Lorem ipsum dolor sit amet, ne has officiis inciderint',
+        link: '#',
+    },
+
+    {
+        logo: <Vogue />,
+        date: 'DEC 28, 2023',
+        title: 'Lorem ipsum dolor sit amet, ne has officiis inciderint',
+        link: '#',
+    },
 ];
 
 export default press;
