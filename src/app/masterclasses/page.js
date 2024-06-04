5bfe31467f2dd567e040f572e75140125454b32e
Novelja <novelja@gmail.com>
2024-06-04 12:46:19 +0200

    masterclasses

--- src/app/masterclasses/page.js
+++ src/app/masterclasses/page.js
@@ -42,6 +42,8 @@ export default function Masterclasses() {
                 setNextLesson(masterclasses[0]);
             }
         }
+
+        console.log('selected: ', selected);
     }, [selected]);
 
     const onNextLessonClick = () => {
--- src/cms/masterclasses.js
+++ src/cms/masterclasses.js
@@ -1,39 +1,83 @@
-import annemarie from '../../assets/judges/annemarie.jpg';
-import clementine from '../../assets/judges/clementine.jpg';
-import martin from '../../assets/judges/martin.jpg';
-
-// import lesson1 from '../../assets/videos/lesson1.mp4';
+import lecture from '../../assets/images/lecture.jpg';
 
 const masterclasses = [
     {
-        thumb: annemarie,
+        thumb: lecture,
         lesson: 'LESSON 1',
-        title: 'Defining Brand Vision',
+        title: 'Defining brand vision',
         copy: `A lasting business has a strong brand identity. In this Masterclass, uncover the creative process behind some of the most disruptive and visionary beauty brands. Hear how our experts find inspiration using moodboards, music, mission statements and more to capture what their brand stands for. `,
         video: '/videos/lesson1.mp4',
     },
 
     {
-        thumb: clementine,
+        thumb: lecture,
         lesson: 'LESSON 2',
-        title: 'Defining Brand Vision',
-        copy: `A lasting business has a strong brand identity. In this Masterclass, uncover the creative process behind some of the most disruptive and visionary beauty brands. Hear how our experts find inspiration using moodboards, music, mission statements and more to capture what their brand stands for. `,
+        title: 'Building a community',
+        copy: `Your community is a key asset in building a beauty brand – but how do you identify and reach them in an authentic way? In this Masterclass, uncover the strategies to leverage your community, plus techniques for communicating with your audience. `,
         video: '/videos/lesson1.mp4',
     },
 
     {
-        thumb: martin,
+        thumb: lecture,
         lesson: 'LESSON 3',
-        title: 'Defining Brand Vision',
-        copy: `A lasting business has a strong brand identity. In this Masterclass, uncover the creative process behind some of the most disruptive and visionary beauty brands. Hear how our experts find inspiration using moodboards, music, mission statements and more to capture what their brand stands for. `,
+        title: 'Brand strategy for growth',
+        copy: `Knowing the white space you want your brand to occupy in the market is critical for every future decision your business will have to make. Are you creating a luxury brand? A sustainable brand? Who is your target customer and where do they shop? Is price your winning factor, or is it packaging? This Masterclass will help you to define your brand strategy, creating a north star for success. `,
         video: '/videos/lesson1.mp4',
     },
 
     {
-        thumb: annemarie,
+        thumb: lecture,
         lesson: 'LESSON 4',
-        title: 'Defining Brand Vision',
-        copy: `A lasting business has a strong brand identity. In this Masterclass, uncover the creative process behind some of the most disruptive and visionary beauty brands. Hear how our experts find inspiration using moodboards, music, mission statements and more to capture what their brand stands for. `,
+        title: 'Product development',
+        copy: `What problem are you trying to solve with a new product? Is it right to launch with one hero product or start wit it h a series? And once you’ve defined your product vision, how do you make it a reality? This Masterclass demystifies the product process: our experts guide you through how to find cosmetic scientists to work with, as well as how to choose a manufacturer.`,
+        video: '/videos/lesson1.mp4',
+    },
+
+    {
+        thumb: lecture,
+        lesson: 'LESSON 5',
+        title: 'Retail and distribution',
+        copy: 'When is the right time in the process to engage retailers and stockists? How do you storytell your brand to buyers? This Masterclass, uncovers the strategies for securing your first retailers, as well as how best to secure their support on your brand’s growth journey.',
+        video: '/videos/lesson1.mp4',
+    },
+
+    {
+        thumb: lecture,
+        lesson: 'LESSON 6',
+        title: 'Financial management',
+        copy: 'Managing your cashflow is key to any business, and negotiating payment terms and understanding your margins is a critical part of this. In this Masterclass, our experts will cover the early stage of financing their brands to get them off the ground, sharing where they prioritise expenditures and where they cut costs. ',
+        video: '/videos/lesson1.mp4',
+    },
+
+    {
+        thumb: lecture,
+        lesson: 'LESSON 7',
+        title: 'Fundraising for scale',
+        copy: 'To build a global brand, you may need to fundraise for your business. In this Masterclass, our experts share their experience of fundraising and what investors were looking for in their brand. Learn about the different types of fundraising and help decide what’s right for you. ',
+        video: '/videos/lesson1.mp4',
+    },
+
+    {
+        thumb: lecture,
+        lesson: 'LESSON 8',
+        title: 'Hiring your A-team',
+        copy: 'In the early stages of a beauty business, you’ll need an incredible team to help you get to the next milestone. How will you find them and how do you work with them? In this Masterclass, our experts reveal who is on their teams, and share some of the key roles that can help on the journey to launching your brand.',
+        video: '/videos/lesson1.mp4',
+    },
+
+    {
+        thumb: lecture,
+        lesson: 'LESSON 9',
+        title: 'Storytelling your launch',
+        copy: 'Now you’re ready to launch, how do you engage press, influencers and social media to amplify your brand? What experiences can you create for your early community to get them excited about your new product? This Masterclass goes through the tactics to cut through the noise and create a unique story for your business. ',
+        video: '/videos/lesson1.mp4',
+    },
+
+    {
+        thumb: lecture,
+        lesson: 'LESSON 10',
+        title: 'The founder’s journey',
+        copy: 'Being an entrepreneur is one of the hardest but most rewarding things you can do. In the final Masterclass, our experts share stories of managing their mindsets while running a fast growing company, including overcoming challenges and avoiding burnout – important factors in the long term health of yourself and your company. ',
         video: '/videos/lesson1.mp4',
     },
 ];
