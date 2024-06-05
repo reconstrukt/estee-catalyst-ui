b601698447325c414ebd828190bc1ac687460ab8
Novelja <novelja@gmail.com>
2024-06-05 21:35:19 +0200

    handle save and exit

--- src/components/application-portal/ApplicationPortal.js
+++ src/components/application-portal/ApplicationPortal.js
@@ -35,6 +35,7 @@ export default function ApplicationPortal() {
         step,
         goBack,
         updateApplication,
+        resetApplication,
         values,
     } = useApplicationPortal();
 
@@ -47,6 +48,7 @@ export default function ApplicationPortal() {
         if (!res.success) {
             // TODO handle errs
         } else {
+            resetApplication();
             setDialogOpen(false);
         }
 
