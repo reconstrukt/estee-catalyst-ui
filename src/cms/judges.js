03dda017625a164e51c535689217aafd16b9a44c
Novelja <novelja@gmail.com>
2024-06-11 21:21:57 +0200

    hp jury

--- src/cms/judges.js
+++ src/cms/judges.js
@@ -21,6 +21,13 @@ const judges = [
         includeOnHome: true,
         image: shana,
         name: 'SHANA RANDHAVA',
+        title: (
+            <>
+                SVP, New Incubation Ventures
+                <br />
+                The Estée Lauder Companies
+            </>
+        ),
         copy: `As the head of New Incubation Ventures (NIV), Shana Randhava is responsible for building The Estée Lauder Companies (ELC) early-stage investment and incubation strategy. This new team is building a distinctive capability for ELC to invest in and experiment at scale and create an actionable pipeline of next generation brands for the ELC portfolio. Prior to creating NIV, Shana led the New Business Development team and was responsible for shaping and executing the ELC’s M&A strategy for DECIEM, Dr. Jart+, Forest Essentials, BY Killian and Too Faced. New Incubation Ventures (NIV) is the strategic early-stage investment and incubation arm for The Estée Lauder Companies. `,
     },
 
@@ -28,18 +35,51 @@ const judges = [
         includeOnHome: true,
         image: vanessa,
         name: 'VANESSA CRAFT',
+        title: (
+            <>
+                SVP, New Incubation Ventures
+                <br />
+                The Estée Lauder Companies
+            </>
+        ),
         copy: `As TikTok’s Global Head of Content Partnerships, Vanessa Craft is a champion of next-gen talent. She is responsible for building engaged communities, directing game-changing content, and driving innovative business strategies for TikTok’s top Global partners across Fashion, Entertainment, Lifestyle, Education and Sports – as well as spearheading TikTok's first sponsorship of the 2024 Met Gala.`,
     },
 
     {
         includeOnHome: true,
+        image: drew,
+        name: 'DREW ELLIOTT',
+        title: (
+            <>
+                SVP & Global Creative Director
+                <br />
+                MAC Cosmetics
+            </>
+        ),
+        copy: `Drew Elliott leads M∙A∙C Cosmetics’ creative vision as Senior Vice President, Global Creative Director while also serving as Strategic Creative Advisor for Too Faced Cosmetics. At the creative helm of M·A·C Cosmetics, Drew is responsible for elevating the brand’s creative identity and trendsetting authority across all touch points of the consumer journey including retail, digital, packaging, visual merchandising, and multiplatform experiences.`,
+    },
+
+    {
+        includeOnHome: true,
+        image: drew,
+        name: 'MITCHELL HALLIDAY',
+        title: (
+            <>
+                Founder
+                <br />
+                Made by Mitchell
+            </>
+        ),
+        copy: ``,
+    },
+
+    {
         image: hung,
         name: 'HUNG VANNGO',
         copy: `Hung Vanngo is a Vietnam-born, New York-based makeup artist known for combining glamour with an avant-garde aesthetic. He’s a go-to for prestigious brands such as Louis Vuitton, Marc Jacobs, and Calvin Klein, as well as high profile talent such as Selena Gomez, Bella Hadid, and Gwyneth Paltrow.`,
     },
 
     {
-        includeOnHome: true,
         image: michelle,
         name: 'MICHÈLE LAMY',
         copy: `Michèle Lamy is an artist, entrepreneur, producer, collaborator, performer and the co-founder of Owenscorp, along with her life partner Rick Owens. In 2014, Lamy created 'LAMYLAND,' an umbrella term for her independent creative endeavours, bringing together experimentation, collaboration, storytelling, and creation. She has worked on projects at renowned institutions and art fairs, including the Museum of Contemporary Art, Los Angeles, Frieze London, the Venice Biennale, and more.`,
@@ -99,22 +139,10 @@ const judges = [
         copy: `Courtney is a key member of the founding team of United Talent Agency’s (UTA) Venture team, where she leads the Incubation division. Her role involves assisting top-tier talent in launching businesses from the incubation stage to exit. Courtney is responsible for identifying best-in-class partners, participating in deal stage negotiations, and helping talent develop their product offerings, strategic brand positioning, and go-to-market strategies. She has played a significant role in shaping the brands of Issa Rae, Emma Chamberlain, Priyanka Chopra, Taraji P Henson and others.`,
     },
 
-    // {
-    //     image: '',
-    //     name: 'ISAMAYA FFRENCH',
-    //     copy: ``,
-    // },
-
     {
         image: sallyann,
         name: 'SALLYANN HOUGHTON',
         copy: `Sallyann Houghton is Business Development / Strategic partnership for Fashion / Retail at Epic Games, based at the Epic Games London Innovation Lab, a hub for the creative community. Prior to Epic, Sallyann worked at renowned global content creation studio The Mill, where she headed up a content division – working directly with high profile and luxury brands to create stand out tech-driven creative content, which included AR, VR, and immersive pieces, along with live action and CG animation.`,
-    },
-
-    {
-        image: drew,
-        name: 'DREW ELLIOTT',
-        copy: `Drew Elliott leads M∙A∙C Cosmetics’ creative vision as Senior Vice President, Global Creative Director while also serving as Strategic Creative Advisor for Too Faced Cosmetics. At the creative helm of M·A·C Cosmetics, Drew is responsible for elevating the brand’s creative identity and trendsetting authority across all touch points of the consumer journey including retail, digital, packaging, visual merchandising, and multiplatform experiences.`,
     },
 
     {
