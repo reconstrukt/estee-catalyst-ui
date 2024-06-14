35f46265faf2a9d467d57cc3fb38052bea45d3fa
Novelja <novelja@gmail.com>
2024-06-14 16:56:13 +0200

    just a logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,9 @@
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import Providers from '@/components/providers/Providers';
-import { Box } from '@mui/material';
+import { Box, Stack } from '@mui/material';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import Logo from '../../assets/svg/logo.svg';
 
 export const metadata = {
     title: 'The Catalysts',
@@ -14,7 +15,17 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body>
-                <Box
+                <Stack
+                    sx={{
+                        height: '100vh',
+                        width: '100%',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                >
+                    <Logo width={250} height={250} />
+                </Stack>
+                {/* <Box
                     sx={{
                         position: 'relative',
                         overflowX: 'hidden',
@@ -23,7 +34,7 @@ export default function RootLayout({ children }) {
                     <AppRouterCacheProvider>
                         <Providers>{children}</Providers>
                     </AppRouterCacheProvider>
-                </Box>
+                </Box> */}
             </body>
         </html>
     );
