1d3c6e4c28d55eec8da70818ff1b5a563dfb93f2
Novelja <novelja@gmail.com>
2024-06-24 16:28:45 +0200

    sorted press

--- src/cms/press.js
+++ src/cms/press.js
@@ -17,117 +17,35 @@ import WWD from 'assets/press/WWD.png';
 
 const press = [
     {
-        logo: MarieClaire,
-        date: 'NOV 22, 2022',
-        title: `Marie Claire: Marie Claire's 1st Annual Power List`,
-        link: `https://www.marieclaire.com/culture/power-list-2022/`,
+        logo: VogueBusiness,
+        date: `FEB 9, 2024`,
+        title: `Four factors shaping China’s beauty market in 2024`,
+        link: `https://www.voguebusiness.com/story/beauty/four-factors-shaping-chinas-beauty-market-in-2024`,
     },
 
     {
-        logo: Glossy,
-        date: `NOV 14, 2022`,
-        title: `Glossy 50 2022: The people who shaped fashion and beauty this year`,
-        link: `https://www.glossy.co/beauty/glossy-50-2022-the-people-who-shaped-fashion-and-beauty-this-year/`,
+        logo: Forbes,
+        date: `DEC 31, 2023`,
+        title: `Trends From China’s Domestic Fragrance Disruptors For 2024`,
+        link: `https://www.forbes.com/sites/gemmawilliams/2023/12/31/trends-from-chinas-domestic-fragrance-disruptors-for-2024/?sh=be967904adec`,
     },
-
     {
-        logo: WWD,
-        date: `MAR 27, 2023`,
-        title: `Lauder’s New Incubation Ventures Invests in Next Generation Fragrance Brand Vyrao`,
-        link: `https://wwd.com/beauty-industry-news/fragrance/lauder-steps-wellness-investing-british-fragrance-brand-vyrao-1235582293/`,
+        logo: JingDaily,
+        date: `DEC 21, 2023`,
+        title: `Why is Estée Lauder investing in Chinese fragrance brand Melt Season?`,
+        link: `https://jingdaily.com/posts/why-is-estee-lauder-investing-in-chinese-fragrance-brand-melt-season`,
     },
-
     {
-        logo: BeautyMatter,
-        date: `MAR 29, 2023`,
-        title: `Estée Lauder Takes Minority Stake in Fragrance Start-up Vyrao`,
-        link: `https://beautymatter.com/articles/estee-lauder-takes-minority-stake-in-vyrao`,
+        logo: FashionNetwork,
+        date: `DEC 20, 2023`,
+        title: `Estée Lauder invests in Chinese perfume brand Melt Season`,
+        link: `https://in.fashionnetwork.com/news/Estee-lauder-invests-in-chinese-perfume-brand-melt-season,1588652.html#rick-owens`,
     },
-
-    {
-        logo: CosmeticsBusiness,
-        date: `MAR 28, 2023`,
-        title: `Fragrance brand Vyrao lands minority investment from Estée Lauder`,
-        link: `https://cosmeticsbusiness.com/news/article_page/Fragrance_brand_Vyrao_lands_minority_investment_from_Estee_Lauder/207837`,
-    },
-
     {
         logo: VogueBusiness,
-        date: `MAY 11, 2023`,
-        title: `Why India’s beauty market is turning heads`,
-        link: `https://www.voguebusiness.com/beauty/why-indias-beauty-market-is-turning-heads`,
-    },
-
-    {
-        logo: VogueBusiness,
-        date: `JUN 9, 2023`,
-        title: `Which beauty brands will investors snap up next?`,
-        link: `https://www.voguebusiness.com/beauty/which-beauty-brands-will-investors-snap-up-next`,
-    },
-
-    {
-        logo: BOF,
-        date: `AVG 11, 2023`,
-        title: `The Estée Lauder Companies’ NewIncubation Ventures Seeks the NextGeneration of Beauty Talent in India`,
-        link: `https://www.businessoffashion.com/articles/beauty/estee-lauder-seeks-next-generation-beauty-talent-in-india-beauty-and-you-awards/`,
-    },
-
-    // COMPANY NEWS
-
-    {
-        logo: Happi,
-        date: `JUN 7, 2023`,
-        title: `The Estée Lauder Companies Expands Beauty&You Competition for Indian Beauty Brands`,
-        link: `https://www.happi.com/breaking-news/the-estee-lauder-companies-introduces-beauty-amp-y/`,
-    },
-
-    {
-        logo: BeautyPackaging,
-        date: `JUN 7, 2023`,
-        title: `Estée Lauder Announces Second Edition of Beauty&You Brand Incubator Program`,
-        link: `https://www.beautypackaging.com/contents/view_breaking-news/2023-06-07/estee-lauder-announces-second-edition-of-beautyyou-brand-incubator-program/`,
-    },
-
-    {
-        logo: CosmeticsAndToiletries,
-        date: `JUN 7, 2023`,
-        title: `The Estée Lauder Companies x Nykaa Announce Second Edition of Beauty&You`,
-        link: `https://www.cosmeticsandtoiletries.com/news/companies/news/22864163/the-este-lauder-companies-x-nykaa-announce-second-edition-of-beautyyou`,
-    },
-
-    {
-        logo: IndianRetailer,
-        date: `JUN 8, 2023`,
-        title: `Estee Lauder X Nykaa Launch 2nd Edition of BEAUTY&YOU to Help Next Gen Indian Beauty Brands`,
-        link: `https://www.indianretailer.com/article/retail-business/retail-trends/estee-lauder-x-nykaa-launch-2nd-edition-beautyyou-help-next`,
-    },
-
-    {
-        logo: ForbesIndia,
-        date: `JUN 9, 2023`,
-        title: `Estée Lauder and Nykaa announce the second edition of BEAUTY&YOU`,
-        link: `https://www.forbesindia.com/article/news/estee-lauder-and-nykaa-announce-the-second-edition-of-beautyyou/85611/1`,
-    },
-
-    {
-        logo: GlobalCosmeticsNews,
-        date: `JUN 12, 2023`,
-        title: `THE ESTEE LAUDER COMPANIES TEAMS UP WITH NYKAA TO LAUNCH BEAUTY & YOU PROGRAM`,
-        link: `https://www.globalcosmeticsnews.com/the-estee-lauder-companies-teams-up-with-nykaa-to-launch-beauty-you-program/`,
-    },
-
-    {
-        logo: CosmeticsBusiness,
-        date: `JUN 8, 2023`,
-        title: `Estée Lauder and Nykaa boost Indian beauty brands with second incubator awards`,
-        link: `https://cosmeticsbusiness.com/news/article_page/Estee_Lauder_and_Nykaa_boost_Indian_beauty_brands_with_second_incubator_awards/209371`,
-    },
-
-    {
-        logo: BOF,
-        date: `AUG 11, 2023`,
-        title: `The Estée Lauder Companies’ New Incubation Ventures Seeks the Next Generation of Beauty Talent in India`,
-        link: `https://www.businessoffashion.com/articles/beauty/estee-lauder-seeks-next-generation-beauty-talent-in-india-beauty-and-you-awards/`,
+        date: `DEC 15, 2023`,
+        title: `Estée Lauder invests in Chinese fragrance brand as market challenges persist`,
+        link: `https://www.voguebusiness.com/beauty/estee-lauder-invests-in-chinese-fragrance-brand-as-market-challenges-persist`,
     },
 
     {
@@ -138,45 +56,104 @@ const press = [
     },
 
     {
-        logo: VogueBusiness,
-        date: `DEC 15, 2023`,
-        title: `Estée Lauder invests in Chinese fragrance brand as market challenges persist`,
-        link: `https://www.voguebusiness.com/beauty/estee-lauder-invests-in-chinese-fragrance-brand-as-market-challenges-persist`,
+        logo: BOF,
+        date: `AUG 11, 2023`,
+        title: `The Estée Lauder Companies’ New Incubation Ventures Seeks the Next Generation of Beauty Talent in India`,
+        link: `https://www.businessoffashion.com/articles/beauty/estee-lauder-seeks-next-generation-beauty-talent-in-india-beauty-and-you-awards/`,
     },
-
-    // {
-    //     logo: '',
-    //     date: ``,
-    //     title: ``,
-    //     link: ``,
-    // },
-
     {
-        logo: FashionNetwork,
-        date: `DEC 20, 2023`,
-        title: `Estée Lauder invests in Chinese perfume brand Melt Season`,
-        link: `https://in.fashionnetwork.com/news/Estee-lauder-invests-in-chinese-perfume-brand-melt-season,1588652.html#rick-owens`,
+        logo: BOF,
+        date: `AVG 11, 2023`,
+        title: `The Estée Lauder Companies’ NewIncubation Ventures Seeks the NextGeneration of Beauty Talent in India`,
+        link: `https://www.businessoffashion.com/articles/beauty/estee-lauder-seeks-next-generation-beauty-talent-in-india-beauty-and-you-awards/`,
     },
 
     {
-        logo: JingDaily,
-        date: `DEC 21, 2023`,
-        title: `Why is Estée Lauder investing in Chinese fragrance brand Melt Season?`,
-        link: `https://jingdaily.com/posts/why-is-estee-lauder-investing-in-chinese-fragrance-brand-melt-season`,
+        logo: GlobalCosmeticsNews,
+        date: `JUN 12, 2023`,
+        title: `THE ESTEE LAUDER COMPANIES TEAMS UP WITH NYKAA TO LAUNCH BEAUTY & YOU PROGRAM`,
+        link: `https://www.globalcosmeticsnews.com/the-estee-lauder-companies-teams-up-with-nykaa-to-launch-beauty-you-program/`,
     },
-
     {
-        logo: Forbes,
-        date: `DEC 31, 2023`,
-        title: `Trends From China’s Domestic Fragrance Disruptors For 2024`,
-        link: `https://www.forbes.com/sites/gemmawilliams/2023/12/31/trends-from-chinas-domestic-fragrance-disruptors-for-2024/?sh=be967904adec`,
+        logo: ForbesIndia,
+        date: `JUN 9, 2023`,
+        title: `Estée Lauder and Nykaa announce the second edition of BEAUTY&YOU`,
+        link: `https://www.forbesindia.com/article/news/estee-lauder-and-nykaa-announce-the-second-edition-of-beautyyou/85611/1`,
     },
-
     {
         logo: VogueBusiness,
-        date: `FEB 9, 2024`,
-        title: `Four factors shaping China’s beauty market in 2024`,
-        link: `https://www.voguebusiness.com/story/beauty/four-factors-shaping-chinas-beauty-market-in-2024`,
+        date: `JUN 9, 2023`,
+        title: `Which beauty brands will investors snap up next?`,
+        link: `https://www.voguebusiness.com/beauty/which-beauty-brands-will-investors-snap-up-next`,
+    },
+    {
+        logo: CosmeticsBusiness,
+        date: `JUN 8, 2023`,
+        title: `Estée Lauder and Nykaa boost Indian beauty brands with second incubator awards`,
+        link: `https://cosmeticsbusiness.com/news/article_page/Estee_Lauder_and_Nykaa_boost_Indian_beauty_brands_with_second_incubator_awards/209371`,
+    },
+    {
+        logo: IndianRetailer,
+        date: `JUN 8, 2023`,
+        title: `Estee Lauder X Nykaa Launch 2nd Edition of BEAUTY&YOU to Help Next Gen Indian Beauty Brands`,
+        link: `https://www.indianretailer.com/article/retail-business/retail-trends/estee-lauder-x-nykaa-launch-2nd-edition-beautyyou-help-next`,
+    },
+    {
+        logo: Happi,
+        date: `JUN 7, 2023`,
+        title: `The Estée Lauder Companies Expands Beauty&You Competition for Indian Beauty Brands`,
+        link: `https://www.happi.com/breaking-news/the-estee-lauder-companies-introduces-beauty-amp-y/`,
+    },
+    {
+        logo: BeautyPackaging,
+        date: `JUN 7, 2023`,
+        title: `Estée Lauder Announces Second Edition of Beauty&You Brand Incubator Program`,
+        link: `https://www.beautypackaging.com/contents/view_breaking-news/2023-06-07/estee-lauder-announces-second-edition-of-beautyyou-brand-incubator-program/`,
+    },
+    {
+        logo: CosmeticsAndToiletries,
+        date: `JUN 7, 2023`,
+        title: `The Estée Lauder Companies x Nykaa Announce Second Edition of Beauty&You`,
+        link: `https://www.cosmeticsandtoiletries.com/news/companies/news/22864163/the-este-lauder-companies-x-nykaa-announce-second-edition-of-beautyyou`,
+    },
+
+    {
+        logo: VogueBusiness,
+        date: `MAY 11, 2023`,
+        title: `Why India’s beauty market is turning heads`,
+        link: `https://www.voguebusiness.com/beauty/why-indias-beauty-market-is-turning-heads`,
+    },
+
+    {
+        logo: BeautyMatter,
+        date: `MAR 29, 2023`,
+        title: `Estée Lauder Takes Minority Stake in Fragrance Start-up Vyrao`,
+        link: `https://beautymatter.com/articles/estee-lauder-takes-minority-stake-in-vyrao`,
+    },
+    {
+        logo: CosmeticsBusiness,
+        date: `MAR 28, 2023`,
+        title: `Fragrance brand Vyrao lands minority investment from Estée Lauder`,
+        link: `https://cosmeticsbusiness.com/news/article_page/Fragrance_brand_Vyrao_lands_minority_investment_from_Estee_Lauder/207837`,
+    },
+    {
+        logo: WWD,
+        date: `MAR 27, 2023`,
+        title: `Lauder’s New Incubation Ventures Invests in Next Generation Fragrance Brand Vyrao`,
+        link: `https://wwd.com/beauty-industry-news/fragrance/lauder-steps-wellness-investing-british-fragrance-brand-vyrao-1235582293/`,
+    },
+
+    {
+        logo: MarieClaire,
+        date: 'NOV 22, 2022',
+        title: `Marie Claire: Marie Claire's 1st Annual Power List`,
+        link: `https://www.marieclaire.com/culture/power-list-2022/`,
+    },
+    {
+        logo: Glossy,
+        date: `NOV 14, 2022`,
+        title: `Glossy 50 2022: The people who shaped fashion and beauty this year`,
+        link: `https://www.glossy.co/beauty/glossy-50-2022-the-people-who-shaped-fashion-and-beauty-this-year/`,
     },
 ];
 
